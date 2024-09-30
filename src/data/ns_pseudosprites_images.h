1c170cbc4684962e8211f118793cdd3eae38b36e
Payst <payst.type@gmail.com>
2024-09-30 12:13:56 +0300

    Update ns_pseudosprites_images.h

--- src/data/ns_pseudosprites_images.h
+++ src/data/ns_pseudosprites_images.h
@@ -1,6 +1,9 @@
 #include "../../pret_include/sprite.h"
-#include "../../graphics/naming_screen/l_0.h"
-#include "../../graphics/naming_screen/l_1.h"
+
+extern u8 l_0Tiles[256];
+extern u16 l_0Pal[256];
+extern u8 l_1Tiles[256];
+extern u16 l_1Pal[256];
 
 static const struct SpriteFrameImage NS_SImageTable_LLetter[] = {
     {l_0Tiles, sizeof(l_0Tiles)},
